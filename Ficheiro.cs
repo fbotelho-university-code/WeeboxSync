53e0184ad3a24b37ea9a043540234b0f716a8f83
t0xe <josesilvalei@gmail.com>
2011-05-24 17:16:47 +0100

    test fuckshit

--- Ficheiro.cs
+++ Ficheiro.cs
@@ -2,6 +2,6 @@ using System;
 namespace WeeboxSync {
     public class Ficheiro {
         private Bundle bundle;
-       
+        //TODO - how to rep filestream?
     }
 }
--- FileSystemAbstraction.cs
+++ FileSystemAbstraction.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Security.AccessControl;
 namespace WeeboxSync {
 
@@ -11,6 +12,9 @@ namespace WeeboxSync {
          */
         public void CreateROLink(String origPath, String destPath) {
             throw new System.Exception("Not implemented");
+
+
+
         }
 
         /*
@@ -19,60 +23,43 @@ namespace WeeboxSync {
          */
         public void CreateROFolder(String path) {
             //returns quietly if dir already exists
+
             if (Directory.Exists(path)) return;
+
             Directory.CreateDirectory(path); //TODO - Set folder permissions
+
             //make dir read only
             //DirectoryInfo myDirectoryInfo = new DirectoryInfo(path);
             //DirectorySecurity myDirectorySecurity = myDirectoryInfo.GetAccessControl();
             //myDirectorySecurity.AddAccessRule(new FileSystemAccessRule(user, FileSystemRights.Modify));
         }
 
-        public void CreateFicheiro(Ficheiro ficheiro, String path) {
-            throw new System.Exception("Not implemented");
-            
-        }
-
         /*
          * deletes an empty folder
          */
-        public void DeleteFolder(String path) {
-            if (IsDirectoryEmpty(path))
+        public void DeleteFolder(String path)
+        {
+            if (!IsDirectoryEmpty(path))
+                return;
+
+            try
             {
-                try
-                {
-                    Directory.Delete(path);
-                }
-                catch (Exception e)
-                {
-                    throw e;
-                }
+                Directory.Delete(path);
             }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            
         }
 
         //delete a folder and all its files and sub-folders
         public void DeleteRecursiveFolder(string path)
         {
-            //if (IsDirectoryEmpty(path))
-            //    return;
-
             DirectoryInfo parent = new DirectoryInfo(path);
-
             parent.Delete(true); //recursive delete
-
-            //foreach (var dir in parent.EnumerateDirectories())
-            //{
-            //    DeleteRecursiveFolder(path + "\\" + dir.Name);
-            //}
-
-            //foreach (var file in parent.EnumerateFiles())
-            //{
-            //    file.Delete();
-            //}
-
-            //parent.Delete();
-            
         }
-
+        
         //determine if a directory is empty and eligible for deletion
         public bool IsDirectoryEmpty(string path)
         {
@@ -88,21 +75,30 @@ namespace WeeboxSync {
             {
                 file.Delete();
             }
-
+                
             dirInfo.Delete();
         }
 
         public List<String> GetFicheiroIDSFromFolder(String path) {
             throw new System.Exception("Not implemented");
+            //TODO - how to get MD5 from file stream?
         }
 
-        public void DeleteFicheiroFromFS(String fpath) {
+        public void DeleteFicheiroFromFS(String fpath)
+        {
+            File.Delete(fpath);
+        }
+
+        //Diferencas??
+        public void SaveFicheiroInFS(String path, Ficheiro ficheiro)
+        {
             throw new System.Exception("Not implemented");
-        }
 
-        public void SaveFicheiroInFS(String path, Ficheiro ficheiro) {
+        }
+        public void CreateFicheiro(Ficheiro ficheiro, String path)
+        {
             throw new System.Exception("Not implemented");
-        }
 
+        }
     }
 }
